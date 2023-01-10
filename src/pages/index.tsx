6e61e6001ae7edc9dbfffcd2e6c6a4537428c326
NivYarmus <Nivyarmus@gmail.com>
2023-01-10 15:12:09 +0200

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import { NextPage } from 'next';
+
+import CustomHeader from '../components/CustomHeader';
+
+const Index: NextPage = () => {
+	return (
+		<>
+			<CustomHeader />
+
+			<main className="min-h-screen flex justify-center items-center" dir="rtl">
+				<section className="bg-white shadow-xl flex flex-col w-96 relative justify-end rounded-xl overflow-hidden group">
+					<img src="/manor.jpg" className="transition-all duration-1000 group-hover:blur-md" />
+					<div className="absolute p-10 flex flex-col flex-1 bg-gradient-to-b from-transparent to-white w-full transition-all duration-700 group-hover:scale-125 origin-bottom-right">
+						<h1 className="text-5xl font-bold mt-20">מנור</h1>
+						<p className="">מנור מנדל. אלילינו האחד והיחיד</p>
+					</div>
+				</section>
+			</main>
+		</>
+	);
+};
+
+export default Index;
