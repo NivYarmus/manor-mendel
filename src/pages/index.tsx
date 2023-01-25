5f75e23f9a7e5d9bb16f5e8d387815cc95bbb767
NivYarmus <Nivyarmus@gmail.com>
2023-01-25 11:23:05 +0200

    manor teach

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,13 @@ const Index: NextPage = () => {
         <section className="bg-white shadow-xl flex flex-col w-96 relative justify-end rounded-xl overflow-hidden m-3">
           <img src="/manor-approval.jpg"/>
           <div className="absolute p-10 flex flex-col flex-1 bg-gradient-to-b from-transparent to-white w-full">
-						<h5 className="text-5xl font-bold mt-20">מנור מאשר</h5>
+						<h5 className="text-3xl font-bold mt-20">מנור מאשר</h5>
+					</div>
+        </section>
+        <section className="bg-white shadow-xl flex flex-col w-96 relative justify-end rounded-xl overflow-hidden m-3">
+          <img src="/manor-teach.jpg"/>
+          <div className="absolute p-10 flex flex-col flex-1 bg-gradient-to-b from-transparent to-white w-full">
+						<p>מחנך את דור העתיד</p>
 					</div>
         </section>
 			</main>
