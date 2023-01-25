17e69b9d23141ddee4b20bc546b7b6ba390fe97b
NivYarmus <Nivyarmus@gmail.com>
2023-01-25 11:18:10 +0200

    manor approves

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,19 @@ const Index: NextPage = () => {
 			<CustomHeader />
 
 			<main className="min-h-screen flex justify-center items-center" dir="rtl">
-				<section className="bg-white shadow-xl flex flex-col w-96 relative justify-end rounded-xl overflow-hidden group">
+				<section className="bg-white shadow-xl flex flex-col w-96 relative justify-end rounded-xl overflow-hidden group m-3">
 					<img src="/manor.jpg" className="transition-all duration-1000 group-hover:blur-md" />
 					<div className="absolute p-10 flex flex-col flex-1 bg-gradient-to-b from-transparent to-white w-full transition-all duration-700 group-hover:scale-125 origin-bottom-right">
 						<h1 className="text-5xl font-bold mt-20">מנור</h1>
 						<p className="">מנור מנדל. אלילינו האחד והיחיד</p>
 					</div>
 				</section>
+        <section className="bg-white shadow-xl flex flex-col w-96 relative justify-end rounded-xl overflow-hidden m-3">
+          <img src="/manor-approval.jpg"/>
+          <div className="absolute p-10 flex flex-col flex-1 bg-gradient-to-b from-transparent to-white w-full">
+						<h5 className="text-5xl font-bold mt-20">מנור מאשר</h5>
+					</div>
+        </section>
 			</main>
 		</>
 	);
