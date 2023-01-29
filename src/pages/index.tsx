22930ad985cdb9c4c3df16738641e35c4002e942
NivYarmus <Nivyarmus@gmail.com>
2023-01-29 11:11:13 +0200

    manor go move move

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,15 +15,15 @@ const Index: NextPage = () => {
 						<p className="">מנור מנדל. אלילינו האחד והיחיד</p>
 					</div>
 				</section>
-        <section className="bg-white shadow-xl flex flex-col w-96 relative justify-end rounded-xl overflow-hidden m-3">
-          <img src="/manor-approval.jpg"/>
-          <div className="absolute p-10 flex flex-col flex-1 bg-gradient-to-b from-transparent to-white w-full">
+        <section className="bg-white shadow-xl flex flex-col w-96 relative justify-end rounded-xl overflow-hidden group m-3">
+          <img src="/manor-approval.jpg" className="transition-all duration-1000 group-hover:blur-md" />
+          <div className="absolute p-10 flex flex-col flex-1 bg-gradient-to-b from-transparent to-white w-full transition-all duration-700 group-hover:scale-125 origin-bottom-right">
 						<h5 className="text-3xl font-bold mt-20">מנור מאשר</h5>
 					</div>
         </section>
-        <section className="bg-white shadow-xl flex flex-col w-96 relative justify-end rounded-xl overflow-hidden m-3">
-          <img src="/manor-teach.jpg"/>
-          <div className="absolute p-10 flex flex-col flex-1 bg-gradient-to-b from-transparent to-white w-full">
+        <section className="bg-white shadow-xl flex flex-col w-96 relative justify-end rounded-xl overflow-hidden group m-3">
+          <img src="/manor-teach.jpg" className="transition-all duration-1000 group-hover:blur-md" />
+          <div className="absolute p-10 flex flex-col flex-1 bg-gradient-to-b from-transparent to-white w-full transition-all duration-700 group-hover:scale-125 origin-bottom-right">
 						<p>מחנך את דור העתיד</p>
 					</div>
         </section>
